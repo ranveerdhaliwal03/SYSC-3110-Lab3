0bcd517630f68b84574a90cedc5ba5d04d671e96
ranveerdhaliwal <ranveerdhaliwal.03@gmail.com>
2023-09-17 21:40:10 -0400

    git commit

---
+++ Lab2/src/AddressBook.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AddressBook {
+
+
+    private List<BuddyInfo> buddies;
+
+   public AddressBook(){
+       buddies = new ArrayList<BuddyInfo>();
+   }
+
+
+   public void addBuddy(BuddyInfo bud){
+       buddies.add(bud);
+
+   }
+
+   public void removeBuddy(BuddyInfo bud){
+        buddies.remove(bud);
+   }
+
+
+
+   public static void main(String[] args){
+
+       System.out.println("Address Book");
+
+//       AddressBook a1 = new AddressBook();
+//       BuddyInfo b1 = new BuddyInfo("Homer","123 main st", "613-555-2828");
+//       BuddyInfo b2 = new BuddyInfo("Jerry","55 main st", "613-999-1234");
+//       BuddyInfo b3 = new BuddyInfo("Kramer","5B main st", "613-456-7676");
+//
+//       a1.addBuddy(b1);
+//       a1.addBuddy(b2);
+//       a1.addBuddy(b3);
+
+
+
+
+
+   }
+
+}
